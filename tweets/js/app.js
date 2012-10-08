08c3309474ad1dafc26d6fe742e316dc1bf21c5e
Javier Arce <javierarce@gmail.com>
2012-10-08 18:33:08 +0200

    Adds animation

---
+++ tweets/js/app.js
@@ -0,0 +1,40 @@
+ map = null;
+frames = [];
+
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame    ||
+  window.oRequestAnimationFrame      ||
+  window.msRequestAnimationFrame     ||
+  function( callback ){
+    window.setTimeout(callback, 1000 / 60);
+  };
+})();
+
+function start() {
+
+(function animloop(){
+
+  requestAnimFrame(animloop);
+
+  try {
+    map.panBy([.2, 0]);
+  } catch(e) {
+
+  }
+
+
+
+
+})();
+
+
+}
+
+
+$(function() {
+
+  start();
+});
