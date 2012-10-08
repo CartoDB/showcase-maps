944c722edd7e9135e99b382a884e4d8daae22e25
Javier Arce <javierarce@gmail.com>
2012-10-08 18:45:34 +0200

    test

--- tweets/js/app.js
+++ tweets/js/app.js
@@ -9,7 +9,7 @@ window.requestAnimFrame = (function(){
   window.oRequestAnimationFrame      ||
   window.msRequestAnimationFrame     ||
   function( callback ){
-    window.setTimeout(callback, 1000 / 60);
+    window.setTimeout(callback, 1000 / 40);
   };
 })();
 
